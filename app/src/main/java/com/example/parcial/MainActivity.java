1dce82bf4b7c0a3c69eba7c06bd6675198a9c53d
Luis Adonay <luisadonaypaz2@gmail.com>
2020-09-07 01:09:31 -0600

    cambios
    Signed-off-by: Luis Adonay <luisadonaypaz2@gmail.com>

--- app/src/main/java/com/example/parcial/MainActivity.java
+++ app/src/main/java/com/example/parcial/MainActivity.java
@@ -1,7 +1,5 @@
 package com.example.parcial;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
@@ -21,12 +19,12 @@ public class MainActivity extends Activity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TabHost tbhConversor = (TabHost) findViewById(R.id.tbhParcial1);
+        TabHost tbhConversor = (TabHost) findViewById(R.id.tbhParcial);
         tbhConversor.setup();
         String str = "Universal";
-        tbhConversor.addTab(tbhConversor.newTabSpec(str).setIndicator(str, null).setContent(R.id.tabConversorUniversal));
+        tbhConversor.addTab(tbhConversor.newTabSpec(str).setIndicator(str, null).setContent(R.id.tabUniversal));
         String str2 = "Area";
-        tbhConversor.addTab(tbhConversor.newTabSpec(str2).setIndicator(str2, null).setContent(R.id.tabAreaSuperficie));
+        tbhConversor.addTab(tbhConversor.newTabSpec(str2).setIndicator(str2, null).setContent(R.id.tabArea));
 
     }
 
@@ -36,16 +34,16 @@ public class MainActivity extends Activity {
             TextView temp = (TextView) findViewById(R.id.txtunidades);
             String str = "";
             int unidades = temp.getText().toString().equals(str) ? 1 : Integer.parseInt(temp.getText().toString());
-            TextView temp2 = (TextView) findViewById(R.id.txtcantidad);
-            TextView valor = (TextView) findViewById(R.id.txtvalor);
+            TextView temp2 = (TextView) findViewById(R.id.txtcantidades);
+            TextView valor = (TextView) findViewById(R.id.txtvalores);
             String str2 = "/";
             if (!temp2.getText().toString().equals(str)) {
-                int cantidad = Integer.parseInt(temp2.getText().toString());
-                int cajas = cantidad / unidades;
+                int cantidades = Integer.parseInt(temp2.getText().toString());
+                int cajas = cantidades / unidades;
                 StringBuilder sb = new StringBuilder();
                 sb.append(cajas);
                 sb.append(str2);
-                sb.append(cantidad % unidades);
+                sb.append(cantidades % unidades);
                 valor.setText(sb.toString());
             } else if (!valor.getText().toString().equals(str)) {
                 String[] data = valor.getText().toString().split(str2);
